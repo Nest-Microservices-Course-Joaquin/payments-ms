84e0e6c0c43795f8c991645362311b9bf74df22b
Moilab <moilab.web@gmail.com>
2026-07-11 16:04:07 +0200

    feat(): createPaymentSession

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Get, Post } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
+import Stripe from 'stripe';
 
 @Controller('payments')
 export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
 
   @Post('create-payment-session')
-  createPaymentSession() {
-    return 'createPaymentSession';
+  async createPaymentSession(): Promise<Stripe.Checkout.Session> {
+    return this.paymentsService.createPaymentSession();
   }
 
   @Get('success')
--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -5,4 +5,30 @@ import { envs } from '../config/env.validation';
 @Injectable()
 export class PaymentsService {
   private readonly stripe = new Stripe(envs.STRIPE_SECRET_KEY);
+
+  async createPaymentSession(): Promise<Stripe.Checkout.Session> {
+    const session = await this.stripe.checkout.sessions.create({
+      // Colorcar aquí el ID de mi orden
+      payment_intent_data: {
+        metadata: {},
+      },
+      line_items: [
+        {
+          price_data: {
+            currency: 'usd',
+            product_data: {
+              name: 'T-Shirt',
+            },
+            unit_amount: 2000, // 20.00 USD
+          },
+          quantity: 2,
+        },
+      ],
+      mode: 'payment',
+      success_url: `http://localhost:3003/payments/success`,
+      cancel_url: `http://localhost:3003/payments/cancelled`,
+    });
+
+    return session;
+  }
 }
